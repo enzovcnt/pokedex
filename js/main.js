ac5512c0059385bc96885a15a37cb78f61572e9c
enzo <enzovcnt@gmail.com>
2024-11-21 19:43:42 +0100

    image with no url

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon/charizard'
+const apiUrl = `https://pokeapi.co/api/v2/pokemon/charizard`
 const name = document.querySelector('.name')
 const type = document.querySelector('.type')
 const height = document.querySelector('.height')
 const weight = document.querySelector('.weight')
 const sprite = document.querySelector('.sprite')
+const button = document.querySelector('.bouton')
+
 
 
 function getpokemon(){fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        sprite.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
+        const image = data.sprites.front_default;
+        sprite.src = image;
         name.innerHTML = data.name;
     })
     .catch(error => console.log(error))
@@ -19,6 +22,6 @@ getpokemon();
 
 
 // étape 1 = faire une carte pour un pokemon
-// étape 2 = url dynamique
+// étape 2 = url dynamique > ${} ??
 // étape 3 = recherche possible selon le nom du pokemon
 // étape 4 = passer d'une simple promesse à async et await > m'a lair d'être le plus utilisé

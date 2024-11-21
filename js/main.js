7b0adc2421660e3056d20079ac94b9f5a49e6a01
enzo <enzovcnt@gmail.com>
2024-11-21 18:42:57 +0100

    add image - link with API success

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert("Hello World!");
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/charizard'
+
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+    data.height
+    console.log(data.height);
+})
+
+
+//response.json()

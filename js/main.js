8250bb48aacaf939ec4144f9c683080e5bfdb0f1
enzo <enzovcnt@gmail.com>
2024-11-26 22:41:53 +0100

    dynamic url works but not every time

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const button = document.querySelector('.bouton')
 
 
 
-function getpokemon(){
+function getPokemon(){
         fetch(apiUrl)
                 .then(response => response.json())
                 .then(data => {
@@ -28,12 +28,40 @@ function getpokemon(){
 
 }
 
-getpokemon();
+getPokemon();
 
-button.addEventListener('click', getpokemon); //networkError when I want to click
+
+const pokemonName = document.querySelector('.search').value;
+const apiUrlDynamic = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+
+
+function searchPokemon(){
+    fetch(apiUrlDynamic)
+        .then(response => response.json())
+        .then(data => {
+            const image = data.sprites.front_default;
+            //const imageType = data.types[0].type;
+            sprite.src = image;
+            name.innerHTML = data.name;
+            type1.innerHTML = data.types[0].type.name;
+            type2.innerHTML = data.types[1].type.name;
+            //typeImage.src = imageType;
+            height.innerHTML ='Height :' + data.height;
+            weight.innerHTML ='Weight :' + data.weight;
+        })
+        .catch(error => console.log(error))
+}
+
+
+button.addEventListener('click', searchPokemon);
 
 
 // étape 1 = faire une carte pour un pokemon > ok
 // étape 2 = url dynamique > ${} ??
 // étape 3 = recherche possible selon le nom du pokemon
+// étape 4 = afficher tout les pokemons sur la même page
+// étape 5 = faire table des types
+// étape 6 = faire une page ou menu déroulant pour les objets/baies
+// étpae 7 = mettre toutes les variantes d'un pokemon
 // étape 4 = passer d'une simple promesse à async et await > m'a lair d'être le plus utilisé

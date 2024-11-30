6019f5374351749a978f6e550d308ab8efa75003
enzo <enzovcnt@gmail.com>
2024-11-30 18:15:01 +0100

    issue with the url i can type a name once but not several time

--- js/main.js
+++ js/main.js
@@ -1,58 +1,33 @@
-const apiUrl = `https://pokeapi.co/api/v2/pokemon/mewtwo`
+//const apiUrl = `https://pokeapi.co/api/v2/pokemon/mewtwo`
 const name = document.querySelector('.name')
 const type1 = document.querySelector('.type1')
 const type2 = document.querySelector('.type2')
 const height = document.querySelector('.height')
 const weight = document.querySelector('.weight')
 const sprite = document.querySelector('.sprite')
-const typeImage = document.querySelector('.typeImage')
+//const typeImage = document.querySelector('.typeImage')
 const button = document.querySelector('.bouton')
 
-
-
-function getPokemon(){
-        fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                        const image = data.sprites.front_default;
-                        //const imageType = data.types[0].type;
-                        sprite.src = image;
-                        name.innerHTML = data.name;
-                        type1.innerHTML = data.types[0].type.name;
-                        type2.innerHTML = data.types[1].type.name;
-                        //typeImage.src = imageType;
-                        height.innerHTML ='Height :' + data.height;
-                        weight.innerHTML ='Weight :' + data.weight;
-    })
-    .catch(error => console.log(error))
-
-}
-
-//getPokemon();
-
-
 const pokemonName = document.querySelector('.search').value;
-const apiUrlDynamic = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-
+const apiUrlDynamic = `https://pokeapi.co/api/v2/pokemon/` + pokemonName;
 
 
 function searchPokemon(){
     fetch(apiUrlDynamic)
         .then(response => response.json())
         .then(data => {
-            const image = data.sprites.front_default;
-            sprite.src = image;
             name.innerHTML = data.name;
+            console.log(name.innerHTML);
+            sprite.src = data.sprites.front_default;
+            console.log('image');
             type1.innerHTML = data.types[0].type.name
-
             if (data.types[1]){
                 type2.innerHTML = data.types[1].type.name;
                 console.log ('je suis type 0')
-            }else {
+            }else{
                 type2.innerHTML = '';
-                console.log ('je suis type 1')
+                console.log ('j ai un type')
             }
-
             height.innerHTML ='Height :' + data.height;
             weight.innerHTML ='Weight :' + data.weight;
 
@@ -63,11 +38,35 @@ function searchPokemon(){
 button.addEventListener('click', searchPokemon);
 
 
+//function getPokemon(){
+        //fetch(apiUrl)
+                //.then(response => response.json())
+                //.then(data => {
+                        //const image = data.sprites.front_default;
+                        //const imageType = data.types[0].type;
+                        //sprite.src = image;
+                        //name.innerHTML = data.name;
+                        //type1.innerHTML = data.types[0].type.name;
+                        //type2.innerHTML = data.types[1].type.name;
+                        //typeImage.src = imageType;
+                        //height.innerHTML ='Height :' + data.height;
+                        //weight.innerHTML ='Weight :' + data.weight;
+    //})
+    //.catch(error => console.log(error))
+
+//}
+
+//getPokemon();
+
+
+
+
+
 
 
 // étape 1 = faire une carte pour un pokemon > ok
 // étape 2 = url dynamique > ${} ?? ok
-// étape 3 = recherche possible selon le nom du pokemon
+// étape 3 = recherche possible selon le nom du pokemon ok
 // étape 4 = afficher tout les pokemons sur la même page
 // étape 5 = faire table des types
 // étape 6 = faire une page ou menu déroulant pour les objets/baies
